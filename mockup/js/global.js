e1e29e30618da2a809f45237b0a9e65a539f50d5
danrobinson78 <dlrobin874@gmail.com>
2015-06-10 20:10:44 -0500

    Got basic mobile menu working.

--- mockup/js/global.js
+++ mockup/js/global.js
@@ -7,13 +7,21 @@ $(document).ready(function () {
     $("html").css("height", "150%");
     $("body").css("height", "100%");
     
-    $(window).scroll(function (e) {
-        if ($(this).scrollTop && !($("#page-header").hasClass("collapsed"))) {
-            $("#social-media,#logo-text").fadeOut(time);
-            $("#page-header").addClass("collapsed");
-        } else if (!($(this).scrollTop()) && $("#page-header").hasClass("collapsed")) {
-            $("#page-header").removeClass("collapsed");
-            $("#social-media,#logo-text").fadeIn(time);
-        }
-    });
+    if ($(window).width() >= 1024) {
+        $(window).scroll(function (e) {
+            if ($(this).scrollTop && !($("#page-header").hasClass("collapsed"))) {
+                $("#social-media,#logo-text").fadeOut(time);
+                $("#page-header").addClass("collapsed");
+            } else if (!($(this).scrollTop()) && $("#page-header").hasClass("collapsed")) {
+                $("#page-header").removeClass("collapsed");
+                $("#social-media,#logo-text").fadeIn(time);
+            }
+        });
+    }
+    
+    if ($(window).width() <= 480) {
+        $("#menu-toggle").click(function (e) {
+            $("#menu").fadeToggle(time);
+        });
+    }
 });
